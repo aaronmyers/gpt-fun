62fa1f5899cbe0a41bd4b56ef50cd8ecb45ac80b
aaronmyers <aaron.n.myers@gmail.com>
2023-12-10 13:13:30 -0600

    adding torch functional stuff

--- mnist_basic.py
+++ mnist_basic.py
@@ -23,11 +23,15 @@ with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
 x_train, y_train, x_valid, y_valid = map(
     torch.tensor, (x_train, y_train, x_valid, y_valid)
     )
+n, c = x_train.shape
 
 
 #### WITHOUT torch
 
 bs = 64 # batch size
+lr = 0.5 #learning reatee
+epochs = 2 # number of training steps
+
 weights = torch.randn(784, 10) / math.sqrt(784)
 weights.requires_grad_()
 bias = torch.zeros(10, requires_grad=True)
@@ -53,3 +57,43 @@ print(loss_func(preds, yb))
 def accuracy(out, yb):
     preds = torch.argmax(out, dim=1)
     return (preds == yb).float().mean()
+
+for epoch in range(epochs):
+    for i in range((n - 1) // bs + 1):
+        #         set_trace()
+        start_i = i * bs
+        end_i = start_i + bs
+        xb = x_train[start_i:end_i]
+        yb = y_train[start_i:end_i]
+        pred = model(xb)
+        loss = loss_func(pred, yb)
+
+        loss.backward()
+        with torch.no_grad():
+            weights -= weights.grad * lr
+            bias -= bias.grad * lr
+            weights.grad.zero_()
+            bias.grad.zero_()
+
+print(loss_func(model(xb), yb), accuracy(model(xb), yb))
+
+### USING Functional torch
+import torch.nn.functional as F
+from torch import nn
+
+loss_func = F.cross_entropy
+
+def model_F(xb):
+    return xb @ weights + bias
+
+class Mnist_Logistic(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(784,10) / math.sqrt(784))
+        self.bias = nn.Parameter(torch.zeros(10))
+
+    def forward(self, xb):
+        return xb @ self.weights + self.bias
+
+mnist_model = Mnist_Logistic()
