c78dc800a5d7724b7d53430ce4a0e22d4264791e
aaronmyers <aaron.n.myers@gmail.com>
2023-12-10 13:07:19 -0600

    adding basic func

--- mnist_basic.py
+++ mnist_basic.py
@@ -3,6 +3,7 @@ import requests
 import pickle
 import gzip
 import torch
+import math
 
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
@@ -19,4 +20,36 @@ if not (PATH / FILENAME).exists():
 with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
     ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
 
+x_train, y_train, x_valid, y_valid = map(
+    torch.tensor, (x_train, y_train, x_valid, y_valid)
+    )
 
+
+#### WITHOUT torch
+
+bs = 64 # batch size
+weights = torch.randn(784, 10) / math.sqrt(784)
+weights.requires_grad_()
+bias = torch.zeros(10, requires_grad=True)
+
+def log_softmax(x):
+    return x - x.exp().sum(-1).log().unsqueeze(-1)
+
+def model(xb):
+    return log_softmax(xb @ weights + bias)
+
+xb = x_train[0:bs]  # a mini-batch from x
+preds = model(xb)  # predictions
+print(preds[0], preds.shape)
+
+def nll(input, target):
+    return -input[range(target.shape[0]), target].mean()
+
+loss_func = nll
+
+yb = y_train[0:bs]
+print(loss_func(preds, yb))
+
+def accuracy(out, yb):
+    preds = torch.argmax(out, dim=1)
+    return (preds == yb).float().mean()
