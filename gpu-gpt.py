0b26b8078e7ead902969fbb222c5fc1c184111da
aaronmyers <aaron.n.myers@gmail.com>
2023-12-11 09:41:34 -0600

    updating params

--- gpu-gpt.py
+++ gpu-gpt.py
@@ -14,8 +14,8 @@ learning_rate = 3e-4
 device = 'mps' #running on a mac M1 chip and want to use gpu
 eval_iters = 200
 n_embd = 384
-n_head = 1
-n_layer = 1
+n_head = 2
+n_layer = 2
 dropout = 0.2
 # ------------
 
