a42eca31cc7be33f091ca139d1c5cd39dff5a4f0
Dr. Myers <aaron.n.myers@gmail.com>
2024-07-05 14:34:15 -0500

    adding llama attempt on huggingface'

---
+++ llama.py
@@ -0,0 +1,34 @@
+import transformers
+import torch
+import os
+
+model_id = "meta-llama/Meta-Llama-3-70B-Instruct"
+TOKEN=os.environ.get("HUG_TOKEN")
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    token=TOKEN,
+    device_map="auto",
+)
+
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
+
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+)
+print(outputs[0]["generated_text"][-1])
